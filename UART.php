ff532d84a98cad09d1c73c7744eb563076f2de59
mara-Luc <mara.luciano.ilm@gmail.com>
2025-04-25 23:41:41 -0400

    test database input form

--- UART.php
+++ UART.php
@@ -3,7 +3,7 @@
 $db_host = "localhost";
 $db_username = "root";
 $db_password = "password";
-$db_name = "drink_dispenser";
+$db_name = "ringDB";
 
 // Set the serial port path and baud rate
 //$serial_port = "/dev/ttyAMA10";  // for GPIO pins
