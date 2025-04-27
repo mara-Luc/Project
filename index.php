4530784e8adc7607eb840f90390387c00271faab
mara-Luc <mara.luciano.ilm@gmail.com>
2025-04-26 20:25:23 -0400

    Update index.php

--- index.php
+++ index.php
@@ -9,140 +9,119 @@
     <title>Portals | Login & Registration</title>
 </head>
 <body>
- <div class="wrapper">
-    <nav class="nav">
-        <div class="nav-logo">
-            <p>Portals.</p>
-        </div>
-        <div class="nav-menu" id="navMenu">
-            <ul>
-                <!--
-                <li><a href="#" class="link active">Login</a></li>
-                <li><a href="#" class="link">Monitoring Center</a></li>
-                <li><a href="#" class="link">History</a></li>
-                <li><a href="#" class="link">Logs</a></li>
-                -->
-                <li><a href="monitoring.php" class="link">Monitoring Center</a></li>
-                <li><a href="history.php" class="link">History</a></li>
-                <li><a href="logs.php" class="link">Logs</a></li>
-                
-            </ul>
-        </div>
-        <!--
-        <div class="nav-button">
-            <button class="btn white-btn" id="loginBtn" onclick="login()">Sign In</button>
-            <button class="btn" id="registerBtn" onclick="register()">Sign Up</button>
-        </div>
-        <div class="nav-menu-btn">
-            <i class="bx bx-menu" onclick="myMenuFunction()"></i>
-        </div>
-        -->
-    </nav>
-<!----------------------------- Form box ----------------------------------->    
-    <div class="form-box">
-        
-        <!------------------- login form -------------------------->
-        <form action="login.php" method="POST">
-        <div class="login-container" id="login">
-            <div class="top">
-                <!--<span>Don't have an account? <a href="#" onclick="register()">Sign Up</a></span>-->
-                <header>Login</header>
+    <div class="wrapper">
+        <nav class="nav">
+            <div class="nav-logo">
+                <p>Portals.</p>
             </div>
-            <div class="input-box">
-                <input type="text" class="input-field" placeholder="Username or Email">
-                <i class="bx bx-user"></i>
+            <div class="nav-menu" id="navMenu">
+                <ul>
+                    <li><a href="login.php" class="link active">Login</a></li>
+                    <li><a href="monitoring.php" class="link">Monitoring Center</a></li>
+                    <li><a href="history.php" class="link">History</a></li>
+                    <li><a href="logs.php" class="link">Logs</a></li>
+                </ul>
             </div>
-            <div class="input-box">
-                <input type="password" class="input-field" placeholder="Password">
-                <i class="bx bx-lock-alt"></i>
-            </div>
-            <div class="input-box">
-                <input type="submit" class="submit" value="Sign In">
-            </div>
-            <!--
-            <div class="two-col">
-                <div class="one">
-                    <input type="checkbox" id="login-check">
-                    <label for="login-check"> Remember Me</label>
+        </nav>
+        <!----------------------------- Form box ----------------------------------->    
+        <div class="form-box">
+            <!------------------- login form -------------------------->
+            <form action="login.php" method="POST">
+                <div class="login-container" id="login">
+                    <div class="top">
+                        <header>Login</header>
+                    </div>
+                    <div class="input-box">
+                        <label for="username">Username:</label>
+                        <input type="text" id="username" name="username" class="input-field" placeholder="Username or Email" required>
+                        <i class="bx bx-user"></i>
+                    </div>
+                    <div class="input-box">
+                        <label for="password">Password:</label>
+                        <input type="password" id="password" name="password" class="input-field" placeholder="Password" required>
+                        <i class="bx bx-lock-alt"></i>
+                    </div>
+                    <div class="input-box">
+                        <input type="submit" class="submit" value="Sign In">
+                    </div>
                 </div>
-                <div class="two">
-                    <label><a href="#">Forgot password?</a></label>
+            </form>
+            <!------------------- registration form -------------------------->
+            <form action="register.php" method="POST">
+                <div class="register-container" id="register">
+                    <div class="top">
+                        <span>Have an account? <a href="login.php" onclick="login()">Login</a></span>
+                        <header>Sign Up</header>
+                    </div>
+                    <div class="two-forms">
+                        <div class="input-box">
+                            <label for="firstname">First Name:</label>
+                            <input type="text" id="firstname" name="firstname" class="input-field" placeholder="Firstname" required>
+                            <i class="bx bx-user"></i>
+                        </div>
+                        <div class="input-box">
+                            <label for="lastname">Last Name:</label>
+                            <input type="text" id="lastname" name="lastname" class="input-field" placeholder="Lastname" required>
+                            <i class="bx bx-user"></i>
+                        </div>
+                    </div>
+                    <div class="input-box">
+                        <label for="email">Email:</label>
+                        <input type="email" id="email" name="email" class="input-field" placeholder="Email" required>
+                        <i class="bx bx-envelope"></i>
+                    </div>
+                    <div class="input-box">
+                        <label for="password">Password:</label>
+                        <input type="password" id="password" name="password" class="input-field" placeholder="Password" required>
+                        <i class="bx bx-lock-alt"></i>
+                    </div>
+                    <div class="input-box">
+                        <input type="submit" class="submit" value="Register">
+                    </div>
+                    <div class="two-col">
+                        <div class="one">
+                            <input type="checkbox" id="register-check">
+                            <label for="register-check"> Remember Me</label>
+                        </div>
+                        <div class="two">
+                            <label><a href="#">Terms & conditions</a></label>
+                        </div>
+                    </div>
                 </div>
-            </div>-->
+            </form>
         </div>
-        <!------------------- registration form -------------------------->
-        <div class="register-container" id="register">
-            <div class="top">
-                <span>Have an account? <a href="#" onclick="login()">Login</a></span>
-                <header>Sign Up</header>
-            </div>
-            <div class="two-forms">
-                <div class="input-box">
-                    <input type="text" class="input-field" placeholder="Firstname">
-                    <i class="bx bx-user"></i>
-                </div>
-                <div class="input-box">
-                    <input type="text" class="input-field" placeholder="Lastname">
-                    <i class="bx bx-user"></i>
-                </div>
-            </div>
-            <div class="input-box">
-                <input type="text" class="input-field" placeholder="Email">
-                <i class="bx bx-envelope"></i>
-            </div>
-            <div class="input-box">
-                <input type="password" class="input-field" placeholder="Password">
-                <i class="bx bx-lock-alt"></i>
-            </div>
-            <div class="input-box">
-                <input type="submit" class="submit" value="Register">
-            </div>
-            <div class="two-col">
-                <div class="one">
-                    <input type="checkbox" id="register-check">
-                    <label for="register-check"> Remember Me</label>
-                </div>
-                <div class="two">
-                    <label><a href="#">Terms & conditions</a></label>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>   
-<script>
-   
-   function myMenuFunction() {
-    var i = document.getElementById("navMenu");
-    if(i.className === "nav-menu") {
-        i.className += " responsive";
-    } else {
-        i.className = "nav-menu";
-    }
-   }
- 
-</script>
-<script>
-    var a = document.getElementById("loginBtn");
-    var b = document.getElementById("registerBtn");
-    var x = document.getElementById("login");
-    var y = document.getElementById("register");
-    function login() {
-        x.style.left = "4px";
-        y.style.right = "-520px";
-        a.className += " white-btn";
-        b.className = "btn";
-        x.style.opacity = 1;
-        y.style.opacity = 0;
-    }
-    function register() {
-        x.style.left = "-510px";
-        y.style.right = "5px";
-        a.className = "btn";
-        b.className += " white-btn";
-        x.style.opacity = 0;
-        y.style.opacity = 1;
-    }
-</script>
+    </div>   
+    <script>
+       function myMenuFunction() {
+           var i = document.getElementById("navMenu");
+           if (i.className === "nav-menu") {
+               i.className += " responsive";
+           } else {
+               i.className = "nav-menu";
+           }
+       }
+    </script>
+    <script>
+        var a = document.getElementById("loginBtn");
+        var b = document.getElementById("registerBtn");
+        var x = document.getElementById("login");
+        var y = document.getElementById("register");
+        function login() {
+            x.style.left = "4px";
+            y.style.right = "-520px";
+            a.className += " white-btn";
+            b.className = "btn";
+            x.style.opacity = 1;
+            y.style.opacity = 0;
+        }
+        function register() {
+            x.style.left = "-510px";
+            y.style.right = "5px";
+            a.className = "btn";
+            b.className += " white-btn";
+            x.style.opacity = 0;
+            y.style.opacity = 1;
+        }
+    </script>
 </body>
 </html>
-CSS Codes
