126fa461cfada98bb15cdaa9cb523f1fb7de09f5
mara-Luc <mara.luciano.ilm@gmail.com>
2025-05-12 03:49:24 -0400

    Update serial_listener.php

--- serial_listener.php
+++ serial_listener.php
@@ -1,5 +1,6 @@
 <?php
 require "php_serial.class.php";
+require "db_connect.php"; // Include your database connection file
 
 $serial = new phpSerial();
 $serial->deviceSet("/dev/ttyUSB0"); // Adjust for your serial device
@@ -9,16 +10,14 @@ $serial->confCharacterLength(8);
 $serial->confStopBits(1);
 $serial->deviceOpen();
 
-$database = new mysqli("localhost", "user", "password", "home_security");
-
 while (true) {
     $input = $serial->readPort();
 
     if ($input == "V") {
         shell_exec("raspivid -o /var/www/html/live.h264 -t 0 &");
-        
+
         // Notify web server
-        $ch = curl_init("http://yourserver.com/notify.php");
+        $ch = curl_init("http://10.0.0.184/notify.php"); //IP of server
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, "event=recording_started");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -31,7 +30,7 @@ while (true) {
         }
 
         // Check UID in database
-        $query = $database->prepare("SELECT pin FROM users WHERE uid = ?");
+        $query = $conn->prepare("SELECT pin FROM users WHERE uid = ?");
         $query->bind_param("s", $uid);
         $query->execute();
         $query->store_result();
@@ -45,12 +44,12 @@ while (true) {
         }
         $query->close();
     } elseif ($input == "W") {
-        shell_exec("pkill raspivid"); // Stop recording
+        shell_exec("pkill -f raspivid"); // Stop recording
     }
 
     usleep(500000); // Wait half a second before checking again
 }
 
 $serial->deviceClose();
-$database->close();
+$conn->close(); // Close the database connection
 ?>
