3a954acdbdcac7c6ee4d43b84289a3e482ca352b
mara-Luc <mara.luciano.ilm@gmail.com>
2025-12-07 01:06:45 -0500

    Update User_Card.php

--- User_Card.php
+++ User_Card.php
@@ -1,14 +1,22 @@
 <?php
-// Reusable function for user cards
+
+// Renders a circular profile card for monitoring center & other pages
 function renderUserCard($picture, $firstname, $lastname, $department) {
-    $imgSrc = "data:image/jpeg;base64," . base64_encode($picture);
-    $fullname = htmlspecialchars($firstname . " " . $lastname);
+
+    // Fallback image if no picture exists
+    if ($picture && strlen($picture) > 0) {
+        $imgSrc = "data:image/jpeg;base64," . base64_encode($picture);
+    } else {
+        $imgSrc = "img/default_user.png"; // place a default icon here
+    }
+
+    $fullname = htmlspecialchars("$firstname $lastname");
     $dept = htmlspecialchars($department);
 
     return "
         <div class='user-card'>
-            <img src='{$imgSrc}' alt='User Picture'>
-            <p>{$fullname}</p>
+            <img src='{$imgSrc}' alt='{$fullname}'>
+            <p class='name'>{$fullname}</p>
             <p class='department'>{$dept}</p>
         </div>
     ";
