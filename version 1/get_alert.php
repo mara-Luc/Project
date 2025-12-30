84cd64cdd358baf6d139bd4c460266d9f09a650f
mara-Luc <mara.luciano.ilm@gmail.com>
2025-12-30 12:46:43 -0500

    cutting fat

--- version 1/get_alert.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-if (isset($_SESSION['security_alert'])) {
-    echo $_SESSION['security_alert'];
-    unset($_SESSION['security_alert']);
-}
-?>
--- version 1/notify.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-$_SESSION['security_alert'] = "⚠️ Someone is attempting to enter your home!";
-echo "OK";
-?>
